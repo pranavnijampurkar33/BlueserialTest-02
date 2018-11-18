7d3d9aafafd0d3127967da8f06a127051434fabe
pratham <pratham.1612@gmail.com>
2018-11-18 11:59:10 +0530

    Initial commit

---
+++ app/src/main/java/org/thewdj/blueserial/core/BlueSerial.java
@@ -0,0 +1,15 @@
+package org.thewdj.blueserial.core;
+
+import android.content.Context;
+
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+
+public class BlueSerial {
+    public static BluetoothSPP instance;
+    public static String addrBuf;
+
+    public static void setInstance(Context context) {
+        instance = new BluetoothSPP(context);
+    }
+
+}
